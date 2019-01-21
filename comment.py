bb2fad93e36064eefb2a1cef5e2d5002cd3a500c
Smith Ringtho <sringtho@gmail.com>
2019-01-21 11:35:05 +0300

    add function for creating a comment

--- comment.py
+++ comment.py
@@ -8,4 +8,14 @@ class Comment:
         self.message = message
         self.timestamp = datetime.today()
         self.author = author
+
+    #function for creating a comment
+    def create_comment(self):
+        comment = dict(
+            message = self.message,
+            timestamp = self.timestamp,
+            author = self.author
+        )
+        comments.append(comment)
+        return comment
   
